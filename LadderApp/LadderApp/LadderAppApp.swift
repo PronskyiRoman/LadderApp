76f75c79160c48ec47d09b054f63a7b8fdf46954
Roman-Pronskyi <92256917+PronskyiRoman@users.noreply.github.com>
2023-01-23 14:19:08 +0200

    initial commit

---
+++ LadderApp/LadderApp/LadderAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LadderAppApp.swift
+//  LadderApp
+//
+//  Created by -_- on 23.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct LadderAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
